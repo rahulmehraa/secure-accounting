14d3ff2ef91b723952514b5c2385aa7bd193f013
rahul <lbmrahulcode@gmail.com>
2023-01-13 14:03:35 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-//import logo from './logo.svg';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Footer from './component/Footer'
 import Button from './component/Button';
 import Routers from './routers/Routers';
+import { useEffect, useState } from 'react';
+import Loader from './component/loader/Loader';
+
 function App() {
+  const [loader , setLoader] = useState(false);
+
+  useEffect(() =>{
+    let timer = setTimeout(() =>{
+      setLoader(true)
+    }, 2000);
+    return () =>{
+      clearTimeout(timer);
+    };
+  }, []);
+  
   return (
-      <div className="App">
+      <div>
+        {
+          !loader ? (
+      <Loader/>
+          ) : (
+            <>
       <Routers />
       <Button />
       <Footer />
+      </>
+          )
+        }
+      
     </div>
   );
 };
---
+++ src/component/loader/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Loader = () =>{
+return(
+  <>
+ <iframe src="https://giphy.com/embed/y1ZBcOGOOtlpC"></iframe>
+    <h3 style={{textAlign:"center"}}>Loading...</h3>
+  </>
+)
+}
+export default Loader ;
+
